5e97f405178a12ea71dfc7538251f225f4d1cb7a
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-05-23 17:32:38 -0600

    Complete

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    number_sentences = self.split(/[(.*)(!*)(?*)]/)
+    new_array = number_sentences.filter {|str| !str.empty?}
+    new_array.count
   end
 end
